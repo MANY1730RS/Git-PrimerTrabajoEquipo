bd72e5f736884ac07db7871d47591e7681ebce68
jmagana38217 <jmagana38217@ucq.edu.mx>
2024-06-13 21:31:52 -0600

    Cheese Burger Resuelve 2

--- Archivo Principal.cpp
+++ Archivo Principal.cpp
@@ -1,14 +1,34 @@
+
+
 #include <iostream>
 #include <string>
 
 using namespace std;
-// Función para verificar si una cadena es un palíndromo
-bool esPalindromo(string cadena) {
-    int longitud = cadena.length();
-    for (int i = 0; i < longitud / 2; i++) {
-        if (cadena[i] != cadena[longitud - 1 - i]) {
+
+
+
+
+
+
+
+
+// Función para verificar si un número es primo
+bool esPrimo(int numero) {
+    if (numero <= 1) {
+        return false;
+    }
+    if (numero <= 3) {
+        return true;
+    }
+    if (numero % 2 == 0 || numero % 3 == 0) {
+        return false;
+    }
+    int i = 5;
+    while (i * i <= numero) {
+        if (numero % i == 0 || numero % (i + 2) == 0) {
             return false;
         }
+        i += 6;
     }
     return true;
 }
@@ -25,21 +45,19 @@ void mostrarMenu() {
 
 void Funcion1()
 {
-cin.ignore(); // Limpiar el buffer de entrada
-                string cadena;
-                cout << "Ingrese una cadena de texto: ";
-                getline(cin, cadena);
-                if (esPalindromo(cadena)) {
-                    cout << cadena << ": En efecto es un palíndromo." << endl;
-                } else {
-                    cout << cadena << ": No es un palíndromo." << endl;
-                }
 
 }
 
 void Funcion2()
 {
-	
+		int numero;
+                cout << "Ingresa un número natural porfa: ";
+                cin >> numero;
+                if (esPrimo(numero)) {
+                    cout << numero << " es lo que se conoce como un número primo" << endl;
+                } else {
+                    cout << numero << " es lo que se conoce como un número compuesto" << endl;
+                }
 }
 
 void Funcion3()
