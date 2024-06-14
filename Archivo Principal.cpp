1dafab360f3d343258e4761dcabea6be48f1458c
jmagana38217 <jmagana38217@ucq.edu.mx>
2024-06-13 21:43:32 -0600

    "Bomba dos Cambios Función Primo"

--- Archivo Principal.cpp
+++ Archivo Principal.cpp
@@ -16,6 +16,27 @@ bool esPalindromo(string cadena) {
     return true;
 }
 
+// Función para verificar si un número es primo
+bool esPrimo(int numero) {
+    if (numero <= 1) {
+        return false;
+    }
+    if (numero <= 3) {
+        return true;
+    }
+    if (numero % 2 == 0 || numero % 3 == 0) {
+        return false;
+    }
+    int i = 5;
+    while (i * i <= numero) {
+        if (numero % i == 0 || numero % (i + 2) == 0) {
+            return false;
+        }
+        i += 6;
+    }
+    return true;
+}
+
 void mostrarMenu() {
     cout << "\nSeleccione una opción: \n";
     cout << "1. Verificar si una cadena es un palíndromo\n";
